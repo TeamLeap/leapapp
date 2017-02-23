243f3a4bb059440be18096d1e5fdde11d495cade
felily <felily@workmail.co.za>
2017-02-22 16:00:11 -0800

    cemetry
    cemetry

--- js/app.js
+++ js/app.js
@@ -2645,6 +2645,95 @@
         }
 
     }]);
+    
+    
+    
+     app.controller('cemetryController', ['$http', '$scope', '$rootScope', '$sce', 'appConfig', 'loadingMessageService', function ($http, $scope, $rootScope, $sce, appConfig, loadingMessageService) {
+
+        $scope.cemetryList = [];
+        $scope.isShowing = false;
+
+        $scope.getCementries = function () {
+
+            $scope.API = appConfig.emmcemetryEndPoint;
+
+            if (typeof $scope.firstname === 'undefined' && typeof $scope.lastname === 'undefined' && typeof $scope.idno === 'undefined') {
+
+                ons.notification.alert({
+                    message: 'This input form not complete!',
+                    modifier: 'material'
+                });
+
+
+            } else {
+
+                $scope.isFetching = true;
+                $rootScope.didYouKnowMessage = loadingMessageService.showMessage();
+                modal.show();
+                
+                if (typeof $scope.firstname === 'undefined')
+                    $scope.firstname = '';
+                else if(typeof $scope.lastname === 'undefined')
+                    $scope.lastname = '';
+                else
+                    $scope.idno = '';
+                
+                $scope.API = $scope.API + '{"FIRSTNAME":"' + $scope.firstname + '","SURNAME":"' + $scope.lastname + '","IDNO":"' + $scope.idno + '"}}';
+
+                $http.get($scope.API).success(function (data) {
+
+                    $scope.cemetryList = [];
+                    
+                    if (typeof data.results === 'undefined')
+                        $scope.cemetryList = [data];
+                    else
+                        $scope.cemetryList = data.results;
+
+                    $scope.isFetching = false;
+                    modal.hide();
+
+                }).error(function (data, status, headers, config) {
+
+                    $scope.isFetching = false;
+                    modal.hide();
+
+                    ons.notification.alert({
+                        message: JSON.stringify('Something went wrong'),
+                        modifier: 'material'
+                    });
+
+                });
+
+            }
+        };
+
+
+    }]);
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     app.controller('statsController', ['$scope', '$rootScope', '$sce', '$http', 'appConfig', function ($scope, $rootScope, $sce, $http, appConfig) {
 
--- js/config.js
+++ js/config.js
@@ -27,6 +27,7 @@ app
   emmIdigentEndPoint : 'http://wmdev.ekurhuleni.gov.za:5555/rest/EMMReporting/resources/indigentMonthlyReports',
   emmHRinfoEndPoint : 'http://wmdev.ekurhuleni.gov.za:5555/rest/EMMHR/resources/getEmployeesPerDept/',
   emmEhealthinfoEndPoint : 'http://wmdev.ekurhuleni.gov.za:5555/rest/EMMEHealth/resources/getMedAssetAmount',
+ emmcemetryEndPoint : 'http://wmdev.ekurhuleni.gov.za:5555/rest/EMMCemetery/resources/personDetails/',
   emmcemetryEndPoint : 'http://wmdev.ekurhuleni.gov.za:5555/rest/EMMCemetery/resources/personDetails/',
   notificationapiEndPoint : 'https://onesignal.com/api/v1/notifications'
 
